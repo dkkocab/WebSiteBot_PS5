4f2f962faa4005ac0305d1cf0fb1095d4a7bed4f
dkkocab <46122733+dkkocab@users.noreply.github.com>
2020-11-20 19:22:36 -0500

    WalmartBot 1.0
    This is the first release of WalmartBot.

--- WalmartBot.py
+++ WalmartBot.py
@@ -1,38 +1,79 @@
 from selenium import webdriver
 import time
 
-
 def order():
+    #VARIABLES
+    addToCart = '//*[@id="add-on-atc-container"]/div[1]/section/div[1]/div[3]/button/span/span'
+    checkOut = '//*[@id="cart-root-container-content-skip"]/div[1]/div/div[2]/div/div/div/div/div[3]/div/div/div[2]/div/div[2]/div/button[1]'
+    continueWithoutAccount = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[1]/div/div/div/div/div[3]/div/div[1]/div/section/section/div/button/span'
+    firstContinue = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[2]/div/div[2]/div/div/div/div[3]/div/div/div[2]/button/span'
+    firstName ='//*[@id="firstName"]'
+    lastName = '//*[@id="lastName"]'
+    email = '//*[@id="email"]'
+    address = '//*[@id="addressLineOne"]'
+    phone = '//*[@id="phone"]'
+    confirmInfo = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[3]/div[1]/div[2]/div/div/div/div[3]/div/div/div/div/div/form/div[2]/div[2]/button/span'
+    creditCardNum = '//*[@id="creditCard"]'
+    creditExpireMonth = '//*[@id="month-chooser"]'
+    creditExpireYear = '//*[@id="year-chooser"]'
+    creditCVV = '//*[@id="cvv"]'
+    reviewOrder = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[4]/div[1]/div[2]/div/div/div/div[3]/div[2]/div/div/div/div[2]/div/div/div/form/div[3]/div/button/span/span/span'
+    confirmOrder = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[2]/div/div/div[2]/div/form/div/button'
+
+    #KEYS
+    #add your information here
+    myFirstName = 'Alex'
+    myLastName = 'Kocab'
+    myEmail = 'dkkocab@gmail.com'
+    myAddress = '5260 Old Hickory Dr'
+    myPhone = '2627947823'
+    myCreditCardNum = '123456789'
+    myCreditExpireMonth = '00'
+    myCreditExpireYear = '25'
+    myCVV = '123'
+
     #ADDS PS5 TO CART AND GOES TO CHECKOUT
-    driver.find_element_by_xpath('//*[@id="add-on-atc-container"]/div[1]/section/div[1]/div[3]/button/span/span').click()
-    time.sleep(2)
-    driver.find_element_by_xpath('//*[@id="cart-root-container-content-skip"]/div[1]/div/div[2]/div/div/div/div/div[3]/div/div/div[2]/div/div[2]/div/button[1]').click()
-    time.sleep(2)
-    driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[1]/div/div/div/div/div[3]/div/div[1]/div/section/section/div/button/span').click()
-    time.sleep(2)
+    clickButton(addToCart)
+    clickButton(checkOut)
+    clickButton(continueWithoutAccount)
 
     #FILLS OUT SHIPPING INFO
-    driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[2]/div/div[2]/div/div/div/div[3]/div/div/div[2]/button/span').click()
-    time.sleep(2)
-    driver.find_element_by_xpath('//*[@id="firstName"]').send_keys('first name')
-    driver.find_element_by_xpath('//*[@id="lastName"]').send_keys('last name')
-    driver.find_element_by_xpath('//*[@id="addressLineOne"]').send_keys('address')
-    driver.find_element_by_xpath('//*[@id="phone"]').send_keys('phone number')
-    driver.find_element_by_xpath('//*[@id="email"]').send_keys('email')
-    time.sleep(3)
-    driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[3]/div[1]/div[2]/div/div/div/div[3]/div/div/div/div/div/form/div[2]/div[2]/button/span').click()
-    time.sleep(2)
-    driver.find_element_by_xpath('//*[@id="creditCard"]').send_keys('credit card')
-    time.sleep(2)
-    
+    clickButton(firstContinue)
+    enterData(firstName, myFirstName)
+    enterData(lastName, myLastName)
+    enterData(phone, myPhone)
+    enterData(email, myEmail)
+    enterData(address, myAddress)
+    clickButton(confirmInfo)
 
+    #FILLS OUT PAYMENT
+    enterData(creditCardNum, myCreditCardNum)
+    enterData(creditExpireMonth, myCreditExpireMonth)
+    enterData(creditExpireYear, myCreditExpireYear)
+    enterData(creditCVV, myCVV)
 
+    #ORDER
+    clickButton(reviewOrder)
+    #clickButton(confirmOrder)
 
+def clickButton(xpath):
+    try:
+        driver.find_element_by_xpath(xpath).click()
+        pass
+    except Exception:
+        time.sleep(1)
+        clickButton(xpath)
+
+def enterData(field,data):
+    try:
+        driver.find_element_by_xpath(field).send_keys(data)
+        pass
+    except Exception:
+        time.sleep(1)
+        enterData(field,data)
 
 if __name__ == "__main__":
     driver = webdriver.Chrome("C:/Users/Alex/Downloads/chromedriver_win32/chromedriver.exe")
-    
-    #driver.get('https://www.walmart.com/ip/PlayStation-5-Console/363472942')
+    #driver.get('https://www.walmart.com/ip/PlayStation-5-Console/363472942') THIS IS THE PS5 LINK
     driver.get('https://www.walmart.com/ip/Sony-3004377-Dualshock-4-Wireless-Controller-for-PlayStation-4-Glacier-White/620764180')
-    time.sleep(1)
     order()
