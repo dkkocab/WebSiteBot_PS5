c74eda7853c242b71a3a59a2cbed4c20e243bc9e
Alex Kocab <46122733+dkkocab@users.noreply.github.com>
2020-11-22 22:06:43 -0500

    Xbox and Page Refresh
    Added URLs that point to Xbox. The page also refreshes every 5 seconds when it can't find the add to cart button. It's kind of slow for refreshing, but it's the fastest I could get it to go without being caught. The UI is GROSS, but it gets the job done. My code is also messy as I didn't rename variables in some places out of laziness, but it works. I plan on cleaning stuff up, but I wanted to push this code as I'm starting to get pull requests

--- WalmartBot_PS5_RELEASE.py
+++ WalmartBot_PS5_RELEASE.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import tkinter
 import time
-from tkinter import Button, messagebox
+from tkinter import Button, Frame, messagebox
 from functools import partial
 
 def order(driver,myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV):
@@ -24,7 +24,7 @@ def order(driver,myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNu
     confirmOrder = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[2]/div/div/div[2]/div/form/div/button'
     
     #ADDS PS5 TO CART AND GOES TO CHECKOUT
-    clickButton(addToCart, driver)
+    clickAddToCartButton(addToCart, driver)
     time.sleep(1)
     clickButton(checkOut, driver)
     time.sleep(1)
@@ -50,6 +50,15 @@ def order(driver,myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNu
     clickButton(reviewOrder)
     clickButton(confirmOrder)
 
+def clickAddToCartButton(xpath,driver):
+    try:
+        driver.find_element_by_xpath(xpath).click()
+        pass
+    except Exception:
+        time.sleep(5)
+        driver.refresh();
+        clickAddToCartButton(xpath, driver)
+
 def clickButton(xpath ,driver):
     try:
         driver.find_element_by_xpath(xpath).click()
@@ -66,84 +75,145 @@ def enterData(field,data ,driver):
         time.sleep(1)
         enterData(field,data, driver)
 
-def PlayStationDigital(myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV):
-    messagebox.showinfo('Message', 'Bot has been turned on. Press "Okay" to continue and the Playstation bot window will close and run in the background...')
-    root.destroy()
-    driver = webdriver.Chrome("C:/Users/Alex/Downloads/chromedriver_win32/chromedriver.exe")
-    driver.get('https://www.walmart.com/ip/Sony-PlayStation-5-Digital-Edition/493824815')
-    order(driver,myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV)
+def PlayStationDigital(myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV, supplier):
+    messagebox.showinfo('Message', 'Bot has been turned on. Press "Okay" to continue and the Playstation bot will run. This application may stop responding as it runs, but it will continue to run the scripts')
+    if supplier == "Walmart":
+        driver = webdriver.Chrome("C:/Users/Alex/Downloads/chromedriver_win32/chromedriver.exe")
+        driver.get('https://www.walmart.com/ip/Sony-PlayStation-5-Digital-Edition/493824815')
+        order(driver,myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV)
 
-def PlayStationPhysical(myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV):
-    messagebox.showinfo('Message', 'Bot has been turned on. Press "Okay" to continue and the Playstation bot window will close and run in the background...')
-    root.destroy()
-    driver = webdriver.Chrome("C:/Users/Alex/Downloads/chromedriver_win32/chromedriver.exe")
-    driver.get('https://www.walmart.com/ip/PlayStation-5-Console/363472942')
-    order(driver,myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV)
+def PlayStationPhysical(myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV, supplier):
+    messagebox.showinfo('Message', 'Bot has been turned on. Press "Okay" to continue and the Playstation bot will run. This application may stop responding as it runs, but it will continue to run the scripts')
+    if supplier == "Walmart":
+        driver = webdriver.Chrome("C:/Users/Alex/Downloads/chromedriver_win32/chromedriver.exe")
+        driver.get('https://www.walmart.com/ip/PlayStation-5-Console/363472942')
+        order(driver,myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV)
+        root.mainloop()
+
+def XboxX(myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV, supplier):
+    messagebox.showinfo('Message', 'Bot has been turned on. Press "Okay" to continue and the Xbox bot will run. This application may stop responding as it runs, but it will continue to run the scripts')
+    if supplier == "Walmart":
+        driver = webdriver.Chrome("C:/Users/Alex/Downloads/chromedriver_win32/chromedriver.exe")
+        driver.get('https://www.walmart.com/ip/XB1-Xbox-Series-X/443574645')
+        order(driver,myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV, supplier)
+        root.mainloop()
+
+def XboxS(myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV, supplier):
+    messagebox.showinfo('Message', 'Bot has been turned on. Press "Okay" to continue and the Xbox bot will run. This application may stop responding as it runs, but it will continue to run the scripts')
+    if supplier == "Walmart":
+        driver = webdriver.Chrome("C:/Users/Alex/Downloads/chromedriver_win32/chromedriver.exe")
+        driver.get('https://www.walmart.com/ip/XB1-Xbox-Series-S/606518560')
+        order(driver,myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV)
+        root.mainloop()
+
+def form(console,supplier):
+    root = tkinter.Tk()
+    root.title("Kocab's PlayStation Bot")
+    root.geometry("1000x500")
+    greeting = tkinter.Label(root, text="*This information isn't needed to run the code. Filling out this information will automate the whole purchase. Leave it blank and it will go to the end of the transcation where you can finish the rest manually*",wraplength=700)
+    greeting.grid(column = 0, row = 0)
+
+    #FORM
+    lableName = tkinter.Label(root, text="First Name: ")
+    lableName.grid(column = 0, row = 10, pady=10)
+    myFirstName = tkinter.StringVar()
+    nameEntered = tkinter.Entry(root, width=25, textvariable = myFirstName)
+    nameEntered.grid(column = 1, row = 10, pady=10)
+
+    lableLast = tkinter.Label(root, text="Last Name: ")
+    lableLast.grid(column = 0, row = 15 ,pady=10)
+    myLastName = tkinter.StringVar()
+    lastEntered = tkinter.Entry(root, width=25, textvariable = myLastName)
+    lastEntered.grid(column = 1, row = 15,padx=10, pady=10)
+
+    lableMail = tkinter.Label(root, text="Email Address: ")
+    lableMail.grid(column = 0, row = 20,padx=10, pady=10)
+    myEmail = tkinter.StringVar()
+    mailEntered = tkinter.Entry(root, width=25, textvariable = myEmail)
+    mailEntered.grid(column = 1, row = 20,padx=10, pady=10)
+
+    lableAddress = tkinter.Label(root, text="Address (ex: 123 Cherry Lane): ")
+    lableAddress.grid(column = 0, row = 25,padx=10, pady=10)
+    myAddress = tkinter.StringVar()
+    addressEntered = tkinter.Entry(root, width=25, textvariable = myAddress)
+    addressEntered.grid(column = 1, row = 25,padx=10, pady=10)
+
+    lablePhone = tkinter.Label(root, text="Phone Number: ")
+    lablePhone.grid(column = 0, row = 30,padx=10, pady=10)
+    myPhone = tkinter.StringVar()
+    PhoneEntered = tkinter.Entry(root, width=25, textvariable = myPhone)
+    PhoneEntered.grid(column = 1, row = 30,padx=10, pady=10)
+
+    lableCardNum = tkinter.Label(root, text="Credit/Debit card number: ")
+    lableCardNum.grid(column = 0, row = 35,padx=10, pady=10)
+    myCreditCardNum = tkinter.StringVar()
+    CardNumEntered = tkinter.Entry(root, width=25, textvariable = myCreditCardNum)
+    CardNumEntered.grid(column = 1, row = 35,padx=10, pady=10)
+
+    lableCreditExpireMonth = tkinter.Label(root, text="Experation Month (00): ")
+    lableCreditExpireMonth.grid(column = 0, row = 40,padx=10, pady=10)
+    myCreditExpireMonth = tkinter.StringVar()
+    CreditExpireMonthEntered = tkinter.Entry(root, width=5, textvariable = myCreditExpireMonth)
+    CreditExpireMonthEntered.grid(column = 1, row = 40,padx=10, pady=10)
+
+    lableCreditExpireYear = tkinter.Label(root, text="Experation Year (00): ")
+    lableCreditExpireYear.grid(column = 0, row = 45,padx=10, pady=10)
+    myCreditExpireYear = tkinter.StringVar()
+    CreditExpireYearEntered = tkinter.Entry(root, width=5, textvariable = myCreditExpireYear)
+    CreditExpireYearEntered.grid(column = 1, row = 45,padx=10, pady=10)
+
+    lableCVV = tkinter.Label(root, text="CVV: ")
+    lableCVV.grid(column = 0, row = 50,padx=10, pady=10)
+    myCVV = tkinter.StringVar()
+    CVVEntered = tkinter.Entry(root, width=5, textvariable = myCVV)
+    CVVEntered.grid(column = 1, row = 50,padx=10, pady=10)
+
+    if console == "PS":
+        if supplier == "Walmart":
+            ps5P = Button(root, text="Playstation Physical", command= partial(PlayStationPhysical ,myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV, "Walmart"))
+            ps5D = Button(root, text="Playstation Digital", command= partial(PlayStationDigital ,myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV, "Walmart"))
+            ps5P.grid(column = 0, row = 55)
+            ps5D.grid(column = 1, row = 55)
+            root.mainloop()
+
+    if console == "XB":
+        if supplier == "Walmart":
+            ps5P = Button(root, text="Xbox Series X", command= partial(XboxX ,myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV, "Walmart"))
+            ps5D = Button(root, text="Xbox Series S", command= partial(XboxS ,myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV, "Walmart"))
+            ps5P.grid(column = 0, row = 55)
+            ps5D.grid(column = 1, row = 55)
+            root.mainloop()
+        
+def PlayStation():
+    root = tkinter.Tk()
+    root.title("Kocab's PlayStation Bot")
+    root.geometry("500x500")
+    pickSupplier = tkinter.Label(root, text="Please chose a supplier")
+    pickSupplier.pack()
+    ps5Start = Button(root, text="Walmart", command= partial(form, "PS" ,"Walmart"))
+    ps5Start.pack()
+    root.mainloop()
+    
+def Xbox():
+    root = tkinter.Tk()
+    root.title("Kocab's PlayStation Bot")
+    root.geometry("500x500")
+    pickSupplier = tkinter.Label(root, text="Please chose a supplier")
+    pickSupplier.pack()
+    ps5Start = Button(root, text="Walmart", command= partial(form, "XB", "Walmart"))
+    ps5Start.pack()
+    root.mainloop()
 
 if __name__ == "__main__":   
     root = tkinter.Tk()
     root.title("Kocab's PlayStation Bot")
     root.geometry("500x500")
-    greeting = tkinter.Label(root, text="Welcome to Kocab's PlayStation 5 Walmart Bot")
-    greeting.grid(column = 25, row = 0,padx=10, pady=10)
-
-    #FORM
-    lableName = tkinter.Label(root, text="First Name: ")
-    lableName.grid(column = 25, row = 10,padx=10, pady=10)
-    myFirstName = tkinter.StringVar()
-    nameEntered = tkinter.Entry(root, width=25, textvariable = myFirstName)
-    nameEntered.grid(column = 26, row = 10,padx=10, pady=10)
-
-    lableLast = tkinter.Label(root, text="Last Name: ")
-    lableLast.grid(column = 25, row = 15,padx=10, pady=10)
-    myLastName = tkinter.StringVar()
-    lastEntered = tkinter.Entry(root, width=25, textvariable = myLastName)
-    lastEntered.grid(column = 26, row = 15,padx=10, pady=10)
-
-    lableMail = tkinter.Label(root, text="Email Address: ")
-    lableMail.grid(column = 25, row = 20,padx=10, pady=10)
-    myEmail = tkinter.StringVar()
-    mailEntered = tkinter.Entry(root, width=25, textvariable = myEmail)
-    mailEntered.grid(column = 26, row = 20,padx=10, pady=10)
-
-    lableAddress = tkinter.Label(root, text="Address (ex: 123 Cherry Lane): ")
-    lableAddress.grid(column = 25, row = 25,padx=10, pady=10)
-    myAddress = tkinter.StringVar()
-    addressEntered = tkinter.Entry(root, width=25, textvariable = myAddress)
-    addressEntered.grid(column = 26, row = 25,padx=10, pady=10)
-
-    lablePhone = tkinter.Label(root, text="Phone Number: ")
-    lablePhone.grid(column = 25, row = 30,padx=10, pady=10)
-    myPhone = tkinter.StringVar()
-    PhoneEntered = tkinter.Entry(root, width=25, textvariable = myPhone)
-    PhoneEntered.grid(column = 26, row = 30,padx=10, pady=10)
-
-    lableCardNum = tkinter.Label(root, text="Credit/Debit card number: ")
-    lableCardNum.grid(column = 25, row = 35,padx=10, pady=10)
-    myCreditCardNum = tkinter.StringVar()
-    CardNumEntered = tkinter.Entry(root, width=25, textvariable = myCreditCardNum)
-    CardNumEntered.grid(column = 26, row = 35,padx=10, pady=10)
-
-    lableCreditExpireMonth = tkinter.Label(root, text="Experation Month (00): ")
-    lableCreditExpireMonth.grid(column = 25, row = 40,padx=10, pady=10)
-    myCreditExpireMonth = tkinter.StringVar()
-    CreditExpireMonthEntered = tkinter.Entry(root, width=5, textvariable = myCreditExpireMonth)
-    CreditExpireMonthEntered.grid(column = 26, row = 40,padx=10, pady=10)
-
-    lableCreditExpireYear = tkinter.Label(root, text="Experation Year (00): ")
-    lableCreditExpireYear.grid(column = 25, row = 45,padx=10, pady=10)
-    myCreditExpireYear = tkinter.StringVar()
-    CreditExpireYearEntered = tkinter.Entry(root, width=5, textvariable = myCreditExpireYear)
-    CreditExpireYearEntered.grid(column = 26, row = 45,padx=10, pady=10)
-
-    lableCVV = tkinter.Label(root, text="CVV: ")
-    lableCVV.grid(column = 25, row = 50,padx=10, pady=10)
-    myCVV = tkinter.StringVar()
-    CVVEntered = tkinter.Entry(root, width=5, textvariable = myCVV)
-    CVVEntered.grid(column = 26, row = 50,padx=10, pady=10)
-
-    ps5P = Button(root, text="Playstation Physical", command= partial(PlayStationPhysical ,myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV))
-    ps5D = Button(root, text="Playstation Digital", command= partial(PlayStationDigital ,myFirstName,myLastName,myEmail,myAddress,myPhone,myCreditCardNum,myCreditExpireMonth,myCreditExpireYear,myCVV))
-    ps5P.grid(column = 25, row = 55)
-    ps5D.grid(column = 26, row = 55)
+    greeting = tkinter.Label(root, text="Welcome to Kocab's PlayStation 5 Bot")
+    greeting.grid(column = 25, row = 0)
+    about1 = tkinter.Label(root, text="This program will run scripts and automates buying a PlayStation 5 or Xbox Series X/S. Please select the console you desire, and you'll be greeted with a form to fill out to automate shipping and payment" ,wraplength=450)
+    about1.grid(column = 25, row = 2,padx=20, pady=10)
+    ps5Start = Button(root, text="Playstation", command= partial(PlayStation))
+    xboxStart = Button(root, text="Xbox", command= partial(Xbox))
+    ps5Start.grid(column = 25, row = 6)
+    xboxStart.grid(column = 25, row = 8)
     root.mainloop()
