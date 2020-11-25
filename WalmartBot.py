f226d1e75955f82c371762ffe50294eba1e72d54
WisdomWolf <wisdomwolf@gmail.com>
2020-11-25 16:59:48 -0500

    Adds option to login if username and password are provided

--- WalmartBot.py
+++ WalmartBot.py
@@ -26,6 +26,8 @@ class WalmartBot:
             credit_month,
             credit_year,
             credit_ccv,
+            username=None,
+            password=None,
             chrome_path=None,
             chrome_path_var=None,
             product_uri=None
@@ -53,58 +55,80 @@ class WalmartBot:
         logger.debug('initialized')
 
     def add_item_to_cart_and_checkout(self):
-        addToCart = '//*[@id="add-on-atc-container"]/div[1]/section/div[1]/div[3]/button/span/span'
-        checkOut = ('//*[@id="cart-root-container-content-skip"]/div[1]/div/div[2]/div/div/div/div/'
-        'div[3]/div/div/div[2]/div/div[2]/div/button[1]')
-        continueWithoutAccount = ('/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[1]'
-        '/div/div/div/div/div[3]/div/div[1]/div/section/section/div/button/span')
+        add_to_cart = '//*[@id="add-on-atc-container"]/div[1]/section/' \
+                    'div[1]/div[3]/button/span/span'
+        check_out = '//*[@id="cart-root-container-content-skip"]/div[1]/div/' \
+                    'div[2]/div/div/div/div/div[3]/div/div/div[2]/div/div[2]/' \
+                    'div/button[1]/div/div/div/div/div[3]/div/div[1]/div/' \
+                    'section/section/div/button/span'
         logger.debug('preparing to add item to cart')
-        self.click_button(addToCart)
+        self.click_button(add_to_cart)
         logger.debug('clicked Add to Cart')
-        self.click_button(checkOut)
+        self.click_button(check_out)
         logger.debug('clicked Checkout')
-        self.click_button(continueWithoutAccount)
+        if self.username and self.password:
+            self.checkout_with_account()
+        else:
+            self.checkout_as_guest()
+
+    def checkout_with_account(self):
+        continue_with_account = '/html/body/div[1]/div/div[1]/div/div[1]/' \
+                                'div[3]/div/div/div/div[1]/div/div/div/div/' \
+                                'div[3]/div/div[4]/div/section/div/section/' \
+                                'form/div[5]/button/span'
+        username = '//*[@id="sign-in-email"]'
+        password = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/' \
+                   'div/div[1]/div/div/div/div/div[3]/div/div[4]/div/' \
+                   'section/div/section/form/div[2]/div/div[1]/label/' \
+                   'div[2]/div/input'
+        self.enter_data(username, self.username)
+        self.enter_data(password, self.password)
+        self.click_button(continue_with_account)
+
+    def checkout_as_guest(self):
+        continue_without_account = '/html/body/div[1]/div/div[1]/div/div[1]/' \
+                                 'div[3]/div/div/div/div[1]'
+        self.click_button(continue_without_account)
         logger.debug('clicked Continue Without Account')
 
     def filling_shipping_info(self):
-        firstContinue = ('/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[2]/div/div[2]/'
-        'div/div/div/div[3]/div/div/div[2]/button/span')
-        first_continue = '/html/body/div[1]/div/div[1]/div/div[2]/div/div/div/div/div[1]/div/button'
         continue_button_div_class = 'CXO_fulfillment_continue'
-        firstName ='//*[@id="firstName"]'
-        lastName = '//*[@id="lastName"]'
+        first_name = '//*[@id="firstName"]'
+        last_name = '//*[@id="lastName"]'
         email = '//*[@id="email"]'
         address = '//*[@id="addressLineOne"]'
         phone = '//*[@id="phone"]'
-        confirmInfo = ('/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[3]/div[1]/div[2]/'
-        'div/div/div/div[3]/div/div/div/div/div/form/div[2]/div[2]/button/span')
+        confirm_info = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/' \
+                       'div/div/div[3]/div[1]/div[2]/div/div/div/div[3]/' \
+                       'div/div/div/div/div/form/div[2]/div[2]/button/span'
         logger.debug('preparing to fill shipping info')
         self.click_button(continue_button_div_class, By.CLASS_NAME)
         logger.debug('clicked on the first continue')
-        self.enter_data(firstName, self.first_name)
-        self.enter_data(lastName, self.last_name)
+        self.enter_data(first_name, self.first_name)
+        self.enter_data(last_name, self.last_name)
         self.enter_data(phone, self.phone)
         self.enter_data(email, self.email)
         self.enter_data(address, self.address)
         logger.debug('shipping information filled')
-        self.click_button(confirmInfo)
+        self.click_button(confirm_info)
         logger.debug('clicked on Confirm')
 
-        '/html/body/div[1]/div/div[1]/div/div[2]/div/div/div/div/div[1]/div/button'
     def fill_out_payment_and_order(self):
-        creditCardNum = '//*[@id="creditCard"]'
-        creditExpireMonth = '//*[@id="month-chooser"]'
-        creditExpireYear = '//*[@id="year-chooser"]'
-        creditCVV = '//*[@id="cvv"]'
-        reviewOrder = ('/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[4]/div[1]/div[2]/div/div'
-        '/div/div[3]/div[2]/div/div/div/div[2]/div/div/div/form/div[3]/div/button/span/span/span')
+        credit_card_num = '//*[@id="creditCard"]'
+        credit_expire_month = '//*[@id="month-chooser"]'
+        credit_expire_year = '//*[@id="year-chooser"]'
+        credit_cvv = '//*[@id="cvv"]'
+        review_order = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/' \
+                       'div/div/div[4]/div[1]/div[2]/div/div/div/div[3]/' \
+                       'div[2]/div/div/div/div[2]/div/div/div/form/div[3]/' \
+                       'div/button/span/span/span'
         logger.debug('preparing to fill payment information')
-        self.enter_data(creditCardNum, self.credit_number)
-        self.enter_data(creditExpireMonth, self.credit_month)
-        self.enter_data(creditExpireYear, self.credit_year)
-        self.enter_data(creditCVV, self.credit_ccv)
+        self.enter_data(credit_card_num, self.credit_number)
+        self.enter_data(credit_expire_month, self.credit_month)
+        self.enter_data(credit_expire_year, self.credit_year)
+        self.enter_data(credit_cvv, self.credit_ccv)
         logger.debug('payment information filled')
-        self.click_button(reviewOrder)
+        self.click_button(review_order)
         logger.debug('clicked Review Order')
 
     def click_button(self, value, method=By.XPATH):
