d76676f4eb01cfd022a81f7c0a0ae68a1b79d703
WisdomWolf <wisdomwolf@gmail.com>
2020-11-25 18:30:19 -0500

    Adds page refresh and more complex element targeting using data-automation-ids

--- WalmartBot.py
+++ WalmartBot.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sys
 import time
 
 from selenium import webdriver
@@ -41,9 +42,11 @@ class WalmartBot:
         self.credit_month = credit_month
         self.credit_year = credit_year
         self.credit_ccv = credit_ccv
+        self.username = username
+        self.password = password
 
         if chrome_path:
-            self.chrome_path = chrome_path
+            self.driver = webdriver.Chrome(chrome_path)
         elif chrome_path_var:
             self.driver = webdriver.Chrome(os.getenv(chrome_path_var))
         else:
@@ -55,16 +58,29 @@ class WalmartBot:
         logger.debug('initialized')
 
     def add_item_to_cart_and_checkout(self):
-        add_to_cart = '//*[@id="add-on-atc-container"]/div[1]/section/' \
-                    'div[1]/div[3]/button/span/span'
-        check_out = '//*[@id="cart-root-container-content-skip"]/div[1]/div/' \
-                    'div[2]/div/div/div/div/div[3]/div/div/div[2]/div/div[2]/' \
-                    'div/button[1]/div/div/div/div/div[3]/div/div[1]/div/' \
-                    'section/section/div/button/span'
+        add_to_cart_xpath = '//*[@id="add-on-atc-container"]/div[1]/section/' \
+                            'div[1]/div[3]/button/span/span'
+        add_to_cart = {
+            'by': By.CLASS_NAME,
+            'value': 'prod-product-cta-add-to-cart',
+            'timeout': 5,
+            'refresh': True
+        }
+        check_out_xpath = {
+            'by': By.XPATH,
+            'value': '//*[@id="cart-root-container-content-skip"]/div[1]/div/'
+                     'div[2]/div/div/div/div/div[3]/div/div/div[2]/div/div[2]/'
+                     'div/button[1]/div/div/div/div/div[3]/div/div[1]/div/'
+                     'section/section/div/button/span'
+        }
+        checkout_div = {
+            'by': By.CLASS_NAME,
+            'value': 'cart-pos-proceed-to-checkout'
+        }
         logger.debug('preparing to add item to cart')
-        self.click_button(add_to_cart)
+        self.click_button(**add_to_cart)
         logger.debug('clicked Add to Cart')
-        self.click_button(check_out)
+        self.click_button(**checkout_div)
         logger.debug('clicked Checkout')
         if self.username and self.password:
             self.checkout_with_account()
@@ -81,18 +97,35 @@ class WalmartBot:
                    'div/div[1]/div/div/div/div/div[3]/div/div[4]/div/' \
                    'section/div/section/form/div[2]/div/div[1]/label/' \
                    'div[2]/div/input'
+        second_continue = {
+            'by': By.CLASS_NAME,
+            'value': 'button--primary'
+        }
         self.enter_data(username, self.username)
         self.enter_data(password, self.password)
         self.click_button(continue_with_account)
+        self.click_button(**second_continue)
 
     def checkout_as_guest(self):
-        continue_without_account = '/html/body/div[1]/div/div[1]/div/div[1]/' \
-                                 'div[3]/div/div/div/div[1]'
-        self.click_button(continue_without_account)
+        continue_without_account_xpath = '/html/body/div[1]/div/div[1]/' \
+                                         'div/div[1]/div[3]/div/div/div/div[1]'
+        continue_without_account = {
+            'by': By.CSS_SELECTOR,
+            'value': "button[data-automation-id="
+                     "new-guest-continue-button]"
+        }
+        self.click_button(**continue_without_account)
         logger.debug('clicked Continue Without Account')
 
-    def filling_shipping_info(self):
-        continue_button_div_class = 'CXO_fulfillment_continue'
+    def fill_shipping_info(self):
+        if self.username and self.password:
+            continue_button = "button[data-automation-id=" \
+                              "address-book-action-buttons-on-continue]"
+            self.click_button(continue_button, By.CSS_SELECTOR)
+        continue_button = {
+            'by': By.CSS_SELECTOR,
+            'value': 'button[data-automation-id=fulfillment-continue]'
+        }
         first_name = '//*[@id="firstName"]'
         last_name = '//*[@id="lastName"]'
         email = '//*[@id="email"]'
@@ -102,7 +135,7 @@ class WalmartBot:
                        'div/div/div[3]/div[1]/div[2]/div/div/div/div[3]/' \
                        'div/div/div/div/div/form/div[2]/div[2]/button/span'
         logger.debug('preparing to fill shipping info')
-        self.click_button(continue_button_div_class, By.CLASS_NAME)
+        self.click_button(**continue_button)
         logger.debug('clicked on the first continue')
         self.enter_data(first_name, self.first_name)
         self.enter_data(last_name, self.last_name)
@@ -131,21 +164,33 @@ class WalmartBot:
         self.click_button(review_order)
         logger.debug('clicked Review Order')
 
-    def click_button(self, value, method=By.XPATH):
+    def click_button(self, value, by=By.XPATH, timeout=1, refresh=False):
         self.check_for_captcha()
         try:
-            button = self.driver.find_element(method, value)
+            button = self.driver.find_element(by, value)
             button.click()
+        except NoSuchElementException:
+            logger.debug('Waiting for element to be available')
+            time.sleep(timeout)
+            if refresh:
+                logger.info('Refreshing page')
+                self.driver.refresh()
+            self.click_button(value, by)
         except Exception:
-            logger.exception('Encountered an error clicking button, will retry in 1s')
-            time.sleep(1)
-            self.click_button(value, method)
+            logger.exception('Encountered an error clicking button, '
+                             'will retry in 1s')
+            time.sleep(timeout)
+            self.click_button(value, by)
 
     def enter_data(self, field, data):
         self.check_for_captcha()
         try:
             self.driver.find_element_by_xpath(field).send_keys(data)
             pass
+        except NoSuchElementException:
+            logger.debug('Waiting for element to be available')
+            time.sleep(1)
+            self.enter_data(field, data)
         except Exception:
             time.sleep(1)
             self.enter_data(field, data)
@@ -195,11 +240,12 @@ if __name__ == "__main__":
     logger.setLevel(logging.DEBUG)
 
     with open('personal_info.yml') as f:
-        personal_info = yaml.load(f)
+        personal_info = yaml.safe_load(f)
     
-    bot = DiaperBot(**personal_info)
+    bot = PS5Bot(**personal_info)
     bot.add_item_to_cart_and_checkout()
-    bot.filling_shipping_info()
-    # bot.fill_out_payment_and_order()
+    bot.fill_shipping_info()
+    bot.fill_out_payment_and_order()
     # You can add those methods in the __init__ as well
     logger.info('FINISHED!')
+    sys.exit(0)
